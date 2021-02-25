b0bb8b896af718abdc46fe5222ea9284c780296c
andycapn <aundreycap@gmail.com>
2021-02-25 11:23:12 +0200

    Feat: Added A Tag to show product tags

--- src/components/productCard.js
+++ src/components/productCard.js
@@ -1,5 +1,9 @@
 import React from "react"
 import { css } from "@emotion/react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faFire } from "@fortawesome/free-solid-svg-icons"
+import New from "../images/new-product.svg"
+import { faAirbnb } from "@fortawesome/free-brands-svg-icons"
 
 const ProductCard = ({ product }) => {
   // Truncate description Text
@@ -15,11 +19,28 @@ const ProductCard = ({ product }) => {
         & * {
           font-family: "Prompt", sans-serif;
         }
-        & > .product-image {
-          height: 60%;
-          width: 100%;
-          object-fit: cover;
+        & > .image-container {
+          position: relative;
+          & > .product-image {
+            height: 60%;
+            width: 100%;
+            object-fit: cover;
+          }
+          & > .tag {
+            position: absolute;
+            top: 0;
+            left: 0;
+            z-index: 5;
+            background-color: yellow;
+            padding: 0.1rem 0.5rem;
+            p {
+              font-size: 12px;
+              margin: 0;
+              padding: 0;
+            }
+          }
         }
+
         & > .content-container {
           padding: 0 10px;
           & > .product-type {
@@ -36,11 +57,31 @@ const ProductCard = ({ product }) => {
         }
       `}
     >
-      <img
-        className="product-image"
-        src={product.images[0].localFile.childImageSharp.fixed.srcWebp}
-        alt={product.title}
-      />
+      <div className="image-container">
+        <img
+          className="product-image"
+          src={product.images[0].localFile.childImageSharp.fixed.srcWebp}
+          alt={product.title}
+        />
+        <div className="tag">
+          <p>
+            <span>
+              <FontAwesomeIcon
+                icon={
+                  product.tags[0] === "Popular"
+                    ? faFire
+                    : product.tags[0] === "New Releases"
+                    ? faAirbnb
+                    : null
+                }
+                style={{ margin: "0 5px" }}
+              />
+              {product.tags[0]}
+            </span>
+          </p>
+        </div>
+      </div>
+
       <div className="content-container">
         <p className="product-type">{product.productType}</p>
         <h5 className="title">{product.title}</h5>
